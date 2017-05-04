11c88bb21b2bc20515fdcdc70497cee8d13871da
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-05-04 01:13:21 -0300

    adding url router for the channelViewset and for CategorielsInChannelList. Also, including api urls into workatolist urls.py, and added docs and simple auth api urls

---
+++ work-at-olist/sales_channels/api/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, include
+from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+from .import views
+
+router = routers.DefaultRouter()
+router.register(r'channels', views.ChannelViewSet)
+router.register(r'categories_in_channel', views.CategoriesInChannelList, base_name='categories_in_channel')
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include_docs_urls(title='Work-at-olist API'))
+]
--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^api/', include('sales_channels.api.urls')),
     url(r'^admin/', admin.site.urls),
 ]
